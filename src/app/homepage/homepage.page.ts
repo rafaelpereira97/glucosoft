db93fd3018ba02a233c907805409b564bd4f7a96
davie <sousinha2511@gmail.com>
2020-11-04 18:02:18 +0000

    Lista com dados

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagePage implements OnInit {
 
+  medicoes: [] =
+      [
+        {
+          Id: 1,
+          ValorMedicao: 127,
+          Data: new Date(),
+},
+        {
+          Id: 2,
+          ValorMedicao: 122,
+          Data: new Date(),
+        },
+        {
+          Id: 3,
+          ValorMedicao: 90,
+          Data: new Date(),
+        },
+        {
+          Id: 4,
+          ValorMedicao: 180,
+          Data: new Date(),
+        },
+        {
+          Id: 4,
+          ValorMedicao: 190,
+          Data: new Date(),
+        },
+          {
+              Id: 5,
+              ValorMedicao: 190,
+              Data: new Date(),
+          },
+          {
+              Id: 6,
+              ValorMedicao: 190,
+              Data: new Date(),
+          },
+
+];
   constructor() { }
 
   ngOnInit() {
+    console.log(this.medicoes);
   }
 
 }
+
