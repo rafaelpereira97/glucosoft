c335af873aed346907241061aad3c2fbcc22a80f
Rafael <rafaelpereira_97@outlook.com>
2020-11-04 20:59:05 +0000

    ...

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -6,48 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.page.scss'],
 })
 export class HomepagePage implements OnInit {
+
     max = 500;
     current = 126;
     color = '';
-  medicoes: [{ Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }] =
-      [
-        {
-          Id: 1,
-          ValorMedicao: 127,
-          Data: new Date(),
-},
-        {
-          Id: 2,
-          ValorMedicao: 122,
-          Data: new Date(),
-        },
-        {
-          Id: 3,
-          ValorMedicao: 90,
-          Data: new Date(),
-        },
-        {
-          Id: 4,
-          ValorMedicao: 180,
-          Data: new Date(),
-        },
-        {
-          Id: 4,
-          ValorMedicao: 190,
-          Data: new Date(),
-        },
-          {
-              Id: 5,
-              ValorMedicao: 190,
-              Data: new Date(),
-          },
-          {
-              Id: 6,
-              ValorMedicao: 190,
-              Data: new Date(),
-          },
 
-];
+      medicoes: [{ Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }, { Data: Date; Id: number; ValorMedicao: number }] =
+          [
+            {
+              Id: 1,
+              ValorMedicao: 127,
+              Data: new Date(),
+            },
+            {
+              Id: 2,
+              ValorMedicao: 122,
+              Data: new Date(),
+            },
+            {
+              Id: 3,
+              ValorMedicao: 90,
+              Data: new Date(),
+            },
+            {
+              Id: 4,
+              ValorMedicao: 180,
+              Data: new Date(),
+            },
+            {
+              Id: 4,
+              ValorMedicao: 190,
+              Data: new Date(),
+            },
+              {
+                  Id: 5,
+                  ValorMedicao: 190,
+                  Data: new Date(),
+              },
+              {
+                  Id: 6,
+                  ValorMedicao: 190,
+                  Data: new Date(),
+              },
+
+    ];
   constructor() { }
 
   ngOnInit() {
