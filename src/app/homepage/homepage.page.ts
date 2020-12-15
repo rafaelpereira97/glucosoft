abc66b22114efadf8c9830f40f28ebafc84943b5
Rafael <rafaelpereira_97@outlook.com>
2020-12-15 00:46:58 +0000

    Inserir registo por sqlite e listagem por sqlite

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -28,7 +28,7 @@ export class HomepagePage implements OnInit {
 
      getAllRegistos(){
           this.db.openDatabaseConnection().then((db: SQLiteObject) => {
-              db.executeSql("SELECT * FROM Registos ORDER BY DateAdd DESC", []).then((data) => {
+              db.executeSql('SELECT * FROM Registos ORDER BY DateAdd DESC', []).then((data) => {
                   this.registos = [];
                   if(data.rows.length > 0) {
                       for(let i = 0; i < data.rows.length; i++) {
